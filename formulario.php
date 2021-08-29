fc79bf8fa264a542cc33f2138462571f65994b40
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2021-08-29 20:18:28 -0300

    subindo funcionalidades

--- formulario.php
+++ formulario.php
@@ -1,28 +1,39 @@
-<?php 
+<?php
 
-if($_POST){
-    $nome = $_POST['nome'];
-    $sobrenome= $_POST['sobrenome'];
 
-    if($nome > 0  && $sobrenome > 0 ){
-         if (is_numeric($nome) && is_numeric($sobrenome)){
-      $complete = $nome. $sobrenome;
-      echo $complete;  
+//variaveis
+$idade = $_POST['idade'];
+$carteira = $_POST['carteira'];
+
+//validação para n mandar em branco
+if ($_POST['idade'] && $_POST['carteira']) {
+    //validando idade e carteira
+    if ($idade >= 16 && $idade <= 65 && $carteira >= 0 && $carteira <= 35 ) {
+        //validando sexo
+        if ($_POST['sexo'] == 'masculino') {
+            //Calculo para homens
+            $calculoMascIdade =  65 - $idade;
+            $calculoMascCarteira =  35 - $carteira;
+            //mostrar na tela homem
+            echo '<p>faltam apenas' . $calculoMascIdade . 'anos' . 'para voce se aposentar</p>';
+            echo '<p>faltam apenas' . $calculoMascCarteira . 'anos em carteira' . 'para voce se aposentar</p>';
+        } else if ($_POST['sexo'] == 'feminino') {
+            //Calculo  para mulher
+            $calculoFemIdade =  62 - $idade;
+            $calculoFemCarteira =  30 - $carteira;
+            // mostrar na tela mulher
+            echo '<p>faltam apenas' . $calculoFemIdade . 'anos' . 'para voce se aposentar</p>';
+            echo '<p>faltam apenas' . $calculoFemCarteira . 'anos em carteira' . 'para voce se aposentar</p>';
+        } else {
+            //fim validação sexo
+            echo 'Nenhum sexo escolhido';
+        }
+    } else {
+        //fim da validação idade
+        echo 'digite uma idade compativel';
     }
-    else{
-        echo "<p>Valor invalido</p>";
-    }
-    }else{
-        echo 'numero n divisivel';
-
-    }
-
-   
-    
-}else{
-    echo '<h1>por favor informar os dados</h1>';
 }
-
-
- 
-?>
+//fim da  validação em branco
+else {
+    echo '<p>por favor preencha os campos</p>';
+}
--- index.php
+++ index.php
@@ -1,21 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='style.css'/>
-    <title>Document</title>
+    <link rel='stylesheet' href="./style/index.css" />
+    <title>Calculo aposentadoria</title>
 </head>
+
 <body>
-    <form action='formulario.php' method='POST'>
-        <label>nome:</label>
-        <input  name='nome' type='text' required/>
-        <label>sobrenome:</label>
-        <input  name='sobrenome' type='text' required/>
-        <button type='submit'>enviar</button>
+    <div>
+        <p>Aplicação para calculo da aposentadoria feito em php</p>
+    </div>
+    <div class="container--form">
+        <form action='formulario.php' method='POST'>
+            <div class="container--style">
+            <div class="input--container">
+                <label>Digite sua idade</label>
+                <input name='idade' type='number' min='16' max='65' />
+            </div>
+            <div  class="input--container">
+                <label> tempo contribuição em carteira</label>
+                <input name='carteira' type='number' min='0' max='35' />
+            </div>
+            <div>
+                <input type='radio' value='masculino' name='sexo' checked />
+                <label>Masculino</label>
+                <input type='radio' value='feminino' name='sexo' />
+                <label>Feminino</label>
+            </div>
+            <div>
+                <button type='submit'>enviar</button>
+            </div>
 
-    </form>
+        </form>
+    </div>
+    </div>
 </body>
-</html>
 
+</html>
